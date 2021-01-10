6ac342d1a4fa262a0ca8870b5b15874f7c60c48f
Mohamed Farhat <mohamedfarhat01@gmail.com>
2021-01-10 14:39:21 +0400

    QueueProducer class for multi messages

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabbitMQProducer
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            QueueProducer.Publish(channel);
+
+
+            /*
+            channel.QueueDeclare("demo-queue", 
+                durable: true, 
+                exclusive: false, 
+                autoDelete: false, 
+                arguments: null);
+            var message = new { Name = "Producer", Message = "Hello!" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+            channel.BasicPublish("", "demo-queue",null,body
+            */
         }
     }
 }
